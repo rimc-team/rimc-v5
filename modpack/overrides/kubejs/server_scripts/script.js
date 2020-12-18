d7592596023db0b05d1b4b66e20505d647cd1e2a
froehlichA <froehlich.alexander@protonmail.com>
2020-12-18 14:13:59 +0100

    add kubejs

---
+++ modpack/overrides/kubejs/server_scripts/script.js
@@ -0,0 +1,7 @@
+events.listen('recipes', event => {
+  // Change recipes here
+})
+
+events.listen('item.tags', event => {
+  // Change item tags here
+})
---
+++ modpack/overrides/kubejs/startup_scripts/script.js
@@ -0,0 +1,9 @@
+events.listen('item.registry', event => {
+  // Register new items here
+  // event.create('example_item').displayName('Example Item')
+})
+
+events.listen('block.registry', event => {
+  // Register new blocks here
+  // event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+})
